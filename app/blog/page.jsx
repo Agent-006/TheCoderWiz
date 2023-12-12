84c809d56e8d62202f1b09645031622370017472
Sagar~ <sagargope2001@gmail.com>
2023-12-12 15:25:32 +0530

    Hero section done and some bug fixed

---
+++ app/blog/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex items-center justify-center h-96 w-full'>
+      Under development
+    </div>
+  )
+}
+
+export default page
